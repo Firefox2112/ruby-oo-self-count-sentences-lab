11f249901021abe236bc43608a84e763cdecbe5d
Coby <cobyrpilpel2@gmail.com>
2020-12-17 06:55:54 -0500

    commit tree 97219ed8112f11d4ada430450e9ac9f560369180

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+     self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[?.!]+/).count ## asked a friend for help on this one. Going to talk to them again and get more info. I think I get it? I don't know if I get it. Not a good sign. 
   end
 end
